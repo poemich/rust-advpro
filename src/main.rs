3b63b92e516c25f5684b33b946e3bf45ff252733
Muhammad Fadhlan Karimuddin <fadhlankarimuddin@gmail.com>
2025-03-21 14:30:39 +0700

    (4) Simulation of slow request

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+	thread,
+	time::Duration,
 };
 
 fn main() {
@@ -21,6 +23,10 @@ fn handle_connection(mut stream: TcpStream) {
     if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "hello.html")
     } 
+	else if request_line == "GET /sleep HTTP/1.1" {
+		thread::sleep(Duration::from_secs(10));
+		("HTTP/1.1 200 OK", "hello.html")
+	}
     else {
         ("HTTP/1.1 404 NOT FOUND", "not_found.html")
     };
